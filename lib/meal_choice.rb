54975dc94897b175ce405e40cbead7c47ddd7314
premti <premti@gmail.com>
2020-02-18 13:36:35 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,9 +2,4 @@
 # For output purposes, use "puts" instead of "print" or "p"
 
 def meal_choice(veg1, veg2, protein)
-  puts "what a nutricious meal."
-  if protein = "meat"
-    puts "#{protein} #{veg1}, #{veg2}"
-else 
-  puts "#{protein} #{veg1}, #{veg2}"
 end
