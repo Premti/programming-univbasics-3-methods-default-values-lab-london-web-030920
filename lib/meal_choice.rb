e0649633b26228df9757be9e2deb43cdb7e11771
premti <premti@gmail.com>
2020-02-18 13:33:52 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,8 +4,4 @@
 def meal_choice(veg1, veg2, protein)
   if protein = "meat"
   puts "A plate of #{protein} with #{veg1} and #{veg2}"
-  elsif protein = "tofu"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}"
-else 
-  puts "What a nutricious meal."
 end
